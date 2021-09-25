35f1b09ab70796c3dd777c481ef014a6bd234b81
rummaxx <91353363+rummaxx@users.noreply.github.com>
2021-09-25 04:08:46 +0400

    Add files via upload

---
+++ NewAttraction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class NewAttraction
+    {
+        public string City;
+        public string Attraction;
+        public string Link;
+        public string Description;
+
+        public NewAttraction(string City, string Attraction, string Link, string Description)
+        {
+            this.City = City;
+            this.Attraction = Attraction;
+            this.Link = Link;
+            this.Description = Description;
+        }
+
+        public String getCity() { return City; }
+        public String getAttraction() { return Attraction; }
+        public String getLink() { return Link; }
+        public String getDescription() { return Description; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        public void getInfo(NewAttraction thisObject)
+        {
+            Console.WriteLine(thisObject.getCity());
+            Console.WriteLine(thisObject.getAttraction());
+            Console.WriteLine(thisObject.getLink());
+            Console.WriteLine(thisObject.getDescription());
+        }
+       
+
+        public void postInfo(String Name, String City,String Attraction,String Link,String Description)
+        {
+            NewAttraction name = new NewAttraction(City, Attraction, Link, Description);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Program start = new Program();
+            start.postInfo("Kreml","Astrakhan", "Kremlin", "http://astrakhan-musei.ru/article/article/view/13843", "Астраханский кремль — историко-архитектурный комплекс,\n филиал ГБУК АО «Астраханский музей-заповедник», \n крепость в Астрахани, выстроенная на месте первых фортификационных сооружений,\n  появившихся в связи с переносом города в 1557-1558 годах воеводой И.С. Черемисиновым.\n  Комплекс Астраханского кремля представляет собой целостный ансамбль памятников оборонного зодчества, \n культовой и гражданской архитектуры и включает в себя 22 объекта XVI — начала XX вв.");
+            Console.Read();
+        }
+    }
+}
